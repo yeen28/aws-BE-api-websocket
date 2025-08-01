db6afea13ff82e57fad47600bf2253560127795f
yeen28 <yestar28@gmail.com>
2025-08-01 17:52:26 +0900

    Revert "task/websocket-java"
    This reverts commit 078762ae3adbbfcaaeb4202303ba72630feec86b.

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # websocket
-
-- Java 21+
-- SpringBoot 3.x.x
-- Gradle
-
-Open API 문서
-- Swagger UI: http://localhost:8080/swagger-ui.html
-- OpenAPI JSON 스펙: http://localhost:8080/api-docs
--- chat-api/src/main/java/com/nameless/social/api/repository/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nameless.social.api.repository;
-
-import com.nameless.social.core.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
