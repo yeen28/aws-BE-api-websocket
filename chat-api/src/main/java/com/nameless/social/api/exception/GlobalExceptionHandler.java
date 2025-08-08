9cb91c3a6a6dd8031514e2bd02227dad5928cd53
yeen28 <yestar28@gmail.com>
2025-08-09 08:39:04 +0900

    bug/배포-시-health-실패-#42
    - Spring Boot Actuator 활성화
    - NoResourceException(favicon icon 없음 등 404에 대한 오류)은 서비스 운영에 영향을 미치지 않기 때문에 로그를 찍지않고 바로 404 응답하도록 수정

--- chat-api/src/main/java/com/nameless/social/api/exception/GlobalExceptionHandler.java
+++ chat-api/src/main/java/com/nameless/social/api/exception/GlobalExceptionHandler.java
@@ -6,14 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(CustomException.class)
 	protected ResponseEntity<CommonResponse<ErrorResponse>> handleCustomException(CustomException e) {
-		log.warn("Custom Exception >>> {}", e.getErrorCode());
-		log.warn("Custom Exception >>> {}", e.getMessage());
+		log.warn("Custom Exception >>> {} - {}", e.getErrorCode(), e.getMessage());
 		final ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
 		return new ResponseEntity<>(CommonResponse.error(errorResponse), e.getErrorCode().getStatus());
 	}
@@ -23,5 +23,11 @@ public class GlobalExceptionHandler {
 		log.warn("Exception >>> {}", e.getMessage());
 		final ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
 		return new ResponseEntity<>(CommonResponse.error(errorResponse), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(NoResourceFoundException.class)
+	protected ResponseEntity<Void> handleNotFound(NoResourceFoundException e) {
+		// 로그 안 찍고 바로 404 응답
+		return ResponseEntity.notFound().build();
 	}
 }
