aws-devlopment-project/websocket

task/DynamoDB에-채팅내역-저장-#4
- AWS DynamoDB v2를 사용하도록 수정

chat-core/src/main/java/com/nameless/social/core/model/MessageModel.java
@@ -1,0 +1,30 @@
+package com.nameless.social.core.model;
+
+import com.nameless.social.core.dto.MessageDto;
+import lombok.Builder;
+import lombok.Getter;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.util.Map;
+
+@Getter
+@Builder
+public class MessageModel {
+	private Long clubId;
+	private String messageId;
+	private String sender;
+	private String message;
+	private String timestamp;
+	private MessageDto.MessageType type;
+
+	public static MessageModel of(Map<String, AttributeValue> item) {
+		return MessageModel.builder()
+				.clubId(Long.parseLong(item.get("clubId").n()))
+				.messageId(item.get("messageId").s())
+				.sender(item.get("sender").s())
+				.message(item.get("message").s())
+				.timestamp(item.get("timestamp").s())
+				.type(MessageDto.MessageType.valueOf(item.get("type").s()))
+				.build();
+	}
+}

chat-websocket/src/main/java/com/nameless/social/websocket/service/ChatMessageService.java
@@ -1,38 +1,91 @@
 package com.nameless.social.websocket.service;
 
-import com.nameless.social.core.dto.ChatMessageDto;
-import com.nameless.social.core.entity.ChatMessage;
-import com.nameless.social.websocket.repository.ChatMessageRepository;
+import com.nameless.social.core.dto.MessageDto;
+import com.nameless.social.core.model.MessageModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
 
-import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatMessageService {
-	private final ChatMessageRepository chatMessageRepository;
+	private final DynamoDbClient dynamoDbClient;
+	private static final String TABLE_NAME = "Message";
 
-	public void saveChatMessage(final ChatMessageDto chatMessageDto) {
-		try {
-			ChatMessage chatMessage = ChatMessage.of(
-					chatMessageDto.getClubId(),
-					chatMessageDto.getSenderId(),
-					chatMessageDto.getMessage()
-			);
+	public void save(final MessageDto messageDto) {
+		log.info("message save start: {}", messageDto.getMessageId());
 
-			chatMessageRepository.save(chatMessage);
-			log.info("Chat message saved to DynamoDB: {}", chatMessage.getMessageId());
+		PutItemRequest request = PutItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.item(Map.of(
+						"messageId", AttributeValue.fromS(messageDto.getMessageId()),
+						"sender", AttributeValue.fromS(messageDto.getSender()),
+						"message", AttributeValue.fromS(messageDto.getMessage()),
+						"timestamp", AttributeValue.fromN(String.valueOf(messageDto.getTimestamp())),
+						"messageType", AttributeValue.fromS(messageDto.getType().name())
+				))
+				.build();
+		dynamoDbClient.putItem(request);
 
-		} catch (Exception e) {
-			log.error("Failed to save chat message to DynamoDB", e);
-			throw new RuntimeException("Failed to save chat message", e);
+		log.info("message saved to DynamoDB: {}", messageDto.getMessage());
+	}
+
+	public MessageModel findByClubId(final long clubId) {
+		GetItemRequest request = GetItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.key(Map.of("clubId", AttributeValue.fromN(String.valueOf(clubId))))
+				.build();
+
+		GetItemResponse response = dynamoDbClient.getItem(request);
+
+		if (response.hasItem()) {
+			Map<String, AttributeValue> item = response.item();
+			return MessageModel.of(item);
+
+		} else {
+			return null;
 		}
 	}
 
-	public List<ChatMessage> getChatHistory(Long chatRoomId) {
-		return chatMessageRepository.findByChatRoomId(chatRoomId);
+	public MessageModel findById(long clubId) {
+		GetItemRequest request = GetItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.key(Map.of("clubId", AttributeValue.fromS(String.valueOf(clubId))))
+				.build();
+
+		GetItemResponse response = dynamoDbClient.getItem(request);
+
+		if (response.hasItem()) {
+			Map<String, AttributeValue> item = response.item();
+			return MessageModel.of(item);
+
+		} else {
+			return null;
+		}
+	}
+
+	public void update(String messageId, String newContent) {
+		UpdateItemRequest request = UpdateItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.key(Map.of("messageId", AttributeValue.fromS(messageId)))
+				.updateExpression("SET content = :content")
+				.expressionAttributeValues(Map.of(":content", AttributeValue.fromS(newContent)))
+				.build();
+
+		dynamoDbClient.updateItem(request);
+	}
+
+	public void delete(String messageId) {
+		DeleteItemRequest request = DeleteItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.key(Map.of("messageId", AttributeValue.fromS(messageId)))
+				.build();
+
+		dynamoDbClient.deleteItem(request);
 	}
 }

yeen28/aws-BE-api-websocket

chat-core/src/main/java/com/nameless/social/core/model/MessageModel.java
@@ -1,0 +1,30 @@
+package com.nameless.social.core.model;
+
+import com.nameless.social.core.dto.ChatPayloadDto;
+import lombok.Builder;
+import lombok.Getter;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.util.Map;
+
+@Getter
+@Builder
+public class MessageModel {
+	private Long clubId;
+	private String messageId;
+	private String sender;
+	private String message;
+	private String timestamp;
+	private ChatPayloadDto.MessageType type;
+
+	public static MessageModel of(Map<String, AttributeValue> item) {
+		return MessageModel.builder()
+				.clubId(Long.parseLong(item.get("clubId").n()))
+				.messageId(item.get("messageId").s())
+				.sender(item.get("sender").s())
+				.message(item.get("message").s())
+				.timestamp(item.get("timestamp").s())
+				.type(ChatPayloadDto.MessageType.valueOf(item.get("type").s()))
+				.build();
+	}
+}

chat-websocket/src/main/java/com/nameless/social/websocket/service/ChatMessageService.java
@@ -1,38 +1,92 @@
 package com.nameless.social.websocket.service;
 
-import com.nameless.social.core.dto.ChatMessageDto;
-import com.nameless.social.core.entity.ChatMessage;
-import com.nameless.social.websocket.repository.ChatMessageRepository;
+import com.nameless.social.core.dto.ChatPayloadDto;
+import com.nameless.social.core.model.MessageModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
 
-import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ChatMessageService {
-	private final ChatMessageRepository chatMessageRepository;
+	private final DynamoDbClient dynamoDbClient;
+	private static final String TABLE_NAME = "Message";
 
-	public void saveChatMessage(final ChatMessageDto chatMessageDto) {
-		try {
-			ChatMessage chatMessage = ChatMessage.of(
-					chatMessageDto.getClubId(),
-					chatMessageDto.getSenderId(),
-					chatMessageDto.getMessage()
-			);
+	public void save(final ChatPayloadDto dto) {
+		log.info("message save start: {}", dto.getMessageId());
 
-			chatMessageRepository.save(chatMessage);
-			log.info("Chat message saved to DynamoDB: {}", chatMessage.getMessageId());
+		PutItemRequest request = PutItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.item(Map.of(
+						"messageId", AttributeValue.fromS(dto.getMessageId()),
+						"senderEmail", AttributeValue.fromS(dto.getSenderEmail()),
+						"senderUsername", AttributeValue.fromS(dto.getSenderUsername()),
+						"message", AttributeValue.fromS(dto.getMessage()),
+						"timestamp", AttributeValue.fromN(String.valueOf(dto.getTimestamp())),
+						"messageType", AttributeValue.fromS(dto.getType().name())
+				))
+				.build();
+		dynamoDbClient.putItem(request);
 
-		} catch (Exception e) {
-			log.error("Failed to save chat message to DynamoDB", e);
-			throw new RuntimeException("Failed to save chat message", e);
+		log.info("message saved to DynamoDB: {}", dto.getMessage());
+	}
+
+	public MessageModel findByClubId(final long clubId) {
+		GetItemRequest request = GetItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.key(Map.of("clubId", AttributeValue.fromN(String.valueOf(clubId))))
+				.build();
+
+		GetItemResponse response = dynamoDbClient.getItem(request);
+
+		if (response.hasItem()) {
+			Map<String, AttributeValue> item = response.item();
+			return MessageModel.of(item);
+
+		} else {
+			return null;
 		}
 	}
 
-	public List<ChatMessage> getChatHistory(Long chatRoomId) {
-		return chatMessageRepository.findByChatRoomId(chatRoomId);
+	public MessageModel findById(long clubId) {
+		GetItemRequest request = GetItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.key(Map.of("clubId", AttributeValue.fromS(String.valueOf(clubId))))
+				.build();
+
+		GetItemResponse response = dynamoDbClient.getItem(request);
+
+		if (response.hasItem()) {
+			Map<String, AttributeValue> item = response.item();
+			return MessageModel.of(item);
+
+		} else {
+			return null;
+		}
+	}
+
+	public void update(String messageId, String newContent) {
+		UpdateItemRequest request = UpdateItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.key(Map.of("messageId", AttributeValue.fromS(messageId)))
+				.updateExpression("SET content = :content")
+				.expressionAttributeValues(Map.of(":content", AttributeValue.fromS(newContent)))
+				.build();
+
+		dynamoDbClient.updateItem(request);
+	}
+
+	public void delete(String messageId) {
+		DeleteItemRequest request = DeleteItemRequest.builder()
+				.tableName(TABLE_NAME)
+				.key(Map.of("messageId", AttributeValue.fromS(messageId)))
+				.build();
+
+		dynamoDbClient.deleteItem(request);
 	}
 }