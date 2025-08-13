9e2a947de221e0c575619de1980a5e39793fbe44
yeen28 <yestar28@gmail.com>
2025-08-13 19:13:59 +0900

    bug/CORS
    - SockJS의 초기 핸드셰이크는 HTTP 요청이므로 OPTIONS 요청을 허용해야 하고, 이 부분이 Spring Security나 ALB에서 차단되면 CORS가 터집니다.

--- chat-api/src/main/java/com/nameless/social/api/handler/WebConfig.java
+++ chat-api/src/main/java/com/nameless/social/api/handler/WebConfig.java
@@ -2,6 +2,7 @@ package com.nameless.social.api.handler;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,20 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(userInfoArgumentResolver);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins(
+						"http://localhost:4200",
+						"https://server.teamnameless.click",
+						"https://stage.teamnameless.click",
+						"https://www.teamnameless.click",
+						"https://teamnameless.click"
+				)
+				.allowedMethods("*")
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
 }
