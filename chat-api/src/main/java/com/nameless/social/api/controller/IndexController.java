f94758e769be0bcb245656e16754f3b479c74e87
yeen28 <yestar28@gmail.com>
2025-08-14 09:40:40 +0900

    task

--- chat-api/src/main/java/com/nameless/social/api/controller/IndexController.java
+++ chat-api/src/main/java/com/nameless/social/api/controller/IndexController.java
@@ -1,13 +1,15 @@
 package com.nameless.social.api.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-// TODO FE와 연동 후 제거
-@Controller
+@RestController
+@RequestMapping("/api")
 public class IndexController {
-	@GetMapping("/api")
+	// 브라우저 직접 접근 /api → index.html 반환
+	@GetMapping({"", "/"})
 	public ModelAndView index() {
 		return new ModelAndView("/index");
 	}
