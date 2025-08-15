6c9125482444b44ef55b4214f041bf7079bd5a8c
yeen28 <yestar28@gmail.com>
2025-08-15 22:00:33 +0900

    bug/websocket 통신시 405 오류 발생
    - 여전히 동일한 오류가 발생하고 있어서 webflux는 임시로 제거

--- chat-websocket/src/main/java/com/nameless/social/websocket/ChatWebsocketApplication.java
+++ chat-websocket/src/main/java/com/nameless/social/websocket/ChatWebsocketApplication.java
@@ -1,7 +1,6 @@
 package com.nameless.social.websocket;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,9 +10,6 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ChatWebsocketApplication {
 	public static void main(String[] args) {
-		// WebFlux 의존성은 WebClient를 사용하는 데만 활용되고, 애플리케이션의 전체 동작은 안정적인 Spring MVC를 기반으로 하게 되어 SockJS 핸드셰이크 문제를 해결
-		SpringApplication application = new SpringApplication(ChatWebsocketApplication.class);
-		application.setWebApplicationType(WebApplicationType.SERVLET);
-		application.run(args);
+		SpringApplication.run(ChatWebsocketApplication.class, args);
 	}
 }
