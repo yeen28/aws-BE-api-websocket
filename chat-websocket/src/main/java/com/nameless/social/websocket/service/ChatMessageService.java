dedb9ac8167d157799de35fb53a8c291a686c2b7
yeen28 <yestar28@gmail.com>
2025-08-15 22:00:33 +0900

    bug/websocket 통신시 405 오류 발생
    - 여전히 동일한 오류가 발생하고 있어서 webflux는 임시로 제거

--- chat-websocket/src/main/java/com/nameless/social/websocket/service/ChatMessageService.java
+++ chat-websocket/src/main/java/com/nameless/social/websocket/service/ChatMessageService.java
@@ -1,14 +1,14 @@
 package com.nameless.social.websocket.service;
 
 import com.nameless.social.core.entity.ChatMessage;
-import com.nameless.social.websocket.client.ChatApiClient;
+//import com.nameless.social.websocket.client.ChatApiClient;
 import com.nameless.social.websocket.config.repository.ChatMessageRepository;
 import com.nameless.social.websocket.dto.ChatPayloadDto;
 import com.nameless.social.websocket.model.MessageModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
+//import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -17,7 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatMessageService {
 	private final ChatMessageRepository chatMessageRepository;
-	private final ChatApiClient chatApiClient;
+//	private final ChatApiClient chatApiClient;
 
 	public void save(final ChatMessage message) {
 		log.info("message save start: {} {}", message.getClubId(), message.getSenderEmail());
@@ -45,22 +45,22 @@ public class ChatMessageService {
 	/**
 	 * 사용자가 club에 가입하면 AI에 정보를 전달해서 퀘스트 생성
 	 */
-	public void sendUserInfoToAI(final ChatPayloadDto dto) {
-		chatApiClient.getUserIdByEmail(dto.getSenderEmail())
-				.flatMap(extractedUserId -> {
-					log.info("Extracted User ID: {}", extractedUserId);
-
-					// TODO: AI 서비스 호출 로직 구현
-					long userId = extractedUserId;
-					long clubId = dto.getClubId();
-
-					return Mono.empty();
-				})
-				.onErrorResume(error -> {
-					// 최종 에러 처리 (ChatApiClient에서 이미 상세 로그를 남겼으므로 여기서는 간단히 로깅)
-					log.error("Error in sendUserInfoToAI for email: {}. Process will not continue.", dto.getSenderEmail());
-					return Mono.empty(); // 스트림을 안전하게 종료
-				})
-				.subscribe();
-	}
+//	public void sendUserInfoToAI(final ChatPayloadDto dto) {
+//		chatApiClient.getUserIdByEmail(dto.getSenderEmail())
+//				.flatMap(extractedUserId -> {
+//					log.info("Extracted User ID: {}", extractedUserId);
+//
+//					// TODO: AI 서비스 호출 로직 구현
+//					long userId = extractedUserId;
+//					long clubId = dto.getClubId();
+//
+//					return Mono.empty();
+//				})
+//				.onErrorResume(error -> {
+//					// 최종 에러 처리 (ChatApiClient에서 이미 상세 로그를 남겼으므로 여기서는 간단히 로깅)
+//					log.error("Error in sendUserInfoToAI for email: {}. Process will not continue.", dto.getSenderEmail());
+//					return Mono.empty(); // 스트림을 안전하게 종료
+//				})
+//				.subscribe();
+//	}
 }
